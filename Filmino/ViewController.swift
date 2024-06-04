629cac307ae40a59d68336aaa23604e599f68394
mfacikgoz <furkanackgz57@yandex.com>
2024-06-04 22:10:43 +0300

    Initial Commit

---
+++ Filmino/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Filmino
+//
+//  Created by Furkan Açıkgöz on 4.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
